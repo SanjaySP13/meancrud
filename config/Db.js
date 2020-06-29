30508a774e92687a5a1b2280dc57eb680bf1e557
SanjaySP13 <65530656+SanjaySP13@users.noreply.github.com>
2020-06-29 13:11:41 +0530

    first commit

---
+++ config/Db.js
@@ -0,0 +1,6 @@
+//db connection string
+
+module.exports={
+    DB:
+    "mongodb+srv://Sansam:sanjay13041999@cluster0-gog5s.mongodb.net/dbCRUD?retryWrites=true&w=majority"
+};
---
+++ model/product.model.js
@@ -0,0 +1,15 @@
+//Database Schema
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+
+let Product=new Schema({
+    title:{type:String},
+    price:{type:Number},
+    image:{type:String},
+    company:{type:String},
+    description:{type:String},
+},{
+    collection:"products",
+});
+
+module.exports=mongoose.model("ProductModel",Product);
